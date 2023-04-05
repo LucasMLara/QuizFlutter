577a806719340de7950122b7bc9bee0b7410041d
Lucas Muniz Lara <lucasml@live.com>
2023-04-05 18:02:26 +0044

    widgets separados

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,62 @@
 import 'package:flutter/material.dart';
-import './questao.dart';
-import './resposta.dart';
+import './questionario.dart';
+import './resultado.dart';
 
-main() => runApp(PerguntaApp());
+void main() => runApp(const PerguntaApp());
 
-class PerguntasAppState extends State<PerguntaApp> {
+class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
-
   final _perguntas = const [
     {
-      'texto': 'Qual é sua cor favorita?',
-      'respostas': ['Preto', 'Vermelho', 'Verde', 'branco']
+      'texto': 'Qual é a sua cor favorita?',
+      'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
     },
     {
-      'texto': 'Qual é seu animal favorito?',
-      'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão']
+      'texto': 'Qual é o seu animal favorito?',
+      'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão'],
     },
     {
-      'texto': 'Qual é seu instrutor favorito?',
-      'respostas': ['Maria', 'João', 'Leo', 'Pedro']
+      'texto': 'Qual é o seu instrutor favorito?',
+      'respostas': ['Maria', 'João', 'Leo', 'Pedro'],
     },
   ];
+
+  void _responder() {
+    if (temPerguntaSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
+      });
+    }
+  }
 
   bool get temPerguntaSelecionada {
     return _perguntaSelecionada < _perguntas.length;
   }
 
-  void _responder() {
-    setState(() {
-      _perguntaSelecionada++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    List<String> respostas = temPerguntaSelecionada
-        ? _perguntas[_perguntaSelecionada].cast()['respostas']
-        : [];
-
-    List<Widget> widgets =
-        respostas.map((t) => Resposta(t, _responder)).toList();
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Perguntas'),
         ),
         body: temPerguntaSelecionada
-            ? Column(
-                children: <Widget>[
-                  Questao(_perguntas[_perguntaSelecionada]['texto'].toString()),
-                  ...widgets
-                ],
+            ? Questionario(
+                perguntas: _perguntas,
+                perguntaSelecionada: _perguntaSelecionada,
+                quandoResponder: _responder,
               )
-            : Center(
-                child: Text(
-                'Parabéns',
-                style: TextStyle(fontSize: 28),
-              )),
+            : const Resultado(),
       ),
     );
   }
 }
 
 class PerguntaApp extends StatefulWidget {
-  PerguntasAppState createState() {
-    return PerguntasAppState();
+  const PerguntaApp({super.key});
+
+  @override
+  _PerguntaAppState createState() {
+    return _PerguntaAppState();
   }
 }
